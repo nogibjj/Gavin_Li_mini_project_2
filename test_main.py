da53d43566018257c6e91d8f11533af155dff952
GLI81 <liguangzheng998@hotmail.com>
2023-09-07 20:53:21 -0400

    Fixed test_main

--- test_main.py
+++ test_main.py
@@ -10,7 +10,7 @@ from main import get_desc_stats#, general_get_desc
 def test_get_desc_stats():
     # print(longestSubstring("abcdcca"))
     assert get_desc_stats().loc["count", "PassengerId"] == 891
-    assert round(get_desc_stats().loc["mean", "survived"], 4) == 0.3838
+    assert round(get_desc_stats().loc["mean", "Survived"], 4) == 0.3838
 
 
 # test_main()
